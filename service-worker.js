a89e1bc68d5bb0feb3ddf9ef23fbe33724174e75
Terra Codes <kehindeowolabi004@gmail.com>
2025-08-14 16:55:41 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'learn-js-single-v1';
+const urlsToCache = ['index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // If found in cache, return it, else fetch from network
+      return response || fetch(event.request);
+    })
+  );
+});
